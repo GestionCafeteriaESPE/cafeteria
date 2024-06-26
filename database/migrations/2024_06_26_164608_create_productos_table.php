53efcb451f9b44387cf6033983995320f1756a2a
Andreamishel <andrea.mcofre@gmail.com>
2024-06-26 14:10:27 -0500

    Subir create_productos_table.php

---
+++ database/migrations/2024_06_26_164608_create_productos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre_pro');
+            $table->string('descripcion_pro');
+            $table->float('precio_pro');
+            $table->string('disponibilidad_pro');
+            $table->string('imagenRef_pro');
+            
+            $table->foreignId('id_categoria')
+                    ->nullable()
+                    ->constrained('categorias')
+                    ->cascadeOnUpdate()
+                    ->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('productos');
+    }
+};
