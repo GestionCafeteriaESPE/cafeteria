c376c59d08e60bc545f8da0a490fd99a0a8c4051
Andreamishel <andrea.mcofre@gmail.com>
2024-07-03 19:42:50 -0500

    Agregar carpeta EgresoResource (acciones del CRUD)

---
+++ app/Filament/Resources/EgresoResource/Pages/CreateEgreso.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\EgresoResource\Pages;
+
+use App\Filament\Resources\EgresoResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateEgreso extends CreateRecord
+{
+    protected static string $resource = EgresoResource::class;
+}
