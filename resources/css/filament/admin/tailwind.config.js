0b9b541842a0ad6b504a5d39cdaf4b97f7f2bbef
ssaloudd <160688800+ssaloudd@users.noreply.github.com>
2024-07-15 01:30:34 -0500

    Reconstrucción de Theme

--- resources/css/filament/admin/tailwind.config.js
+++ resources/css/filament/admin/tailwind.config.js
@@ -1,19 +1,10 @@
-import preset from './vendor/filament/support/tailwind.config.preset';
+import preset from '../../../../vendor/filament/filament/tailwind.config.preset'
 
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/*/.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
- 
 export default {
     presets: [preset],
     content: [
-        './app/Filament/*/.php',
-        './resources/views/filament/*/.blade.php',
-        './vendor/filament/*/.blade.php',
+        './app/Filament/**/*.php',
+        './resources/views/filament/**/*.blade.php',
+        './vendor/filament/**/*.blade.php',
     ],
 }
